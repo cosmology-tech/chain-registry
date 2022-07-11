101dc56feeefc78e78a27b5cd4da1338649b76bb
Dan Lynch <pyramation@gmail.com>
2022-07-10 18:59:40 -0700

    utils

---
+++ packages/chain-registry-utils/src/index.ts
@@ -0,0 +1,108 @@
+const { Hash } = require('@keplr-wallet/crypto');
+
+export const ibcDenom = (
+  paths: {
+    portId: string;
+    channelId: string;
+  }[],
+  coinMinimalDenom: string
+): string => {
+  const prefixes = [];
+  for (const path of paths) {
+    prefixes.push(`${path.portId}/${path.channelId}`);
+  }
+
+  const prefix = prefixes.join("/");
+  const denom = `${prefix}/${coinMinimalDenom}`;
+
+  return (
+    "ibc/" +
+    Buffer.from(Hash.sha256(Buffer.from(denom)))
+      .toString("hex")
+      .toUpperCase()
+  );
+};
+
+const findInfo = (ibc, to, from) =>
+  ibc.find(i =>
+    i['chain-1']['chain-name'] === from
+    &&
+    i['chain-2']['chain-name'] === to
+  );
+
+export interface IBCChainInfo {
+  "chain-name": string;
+  "client-id": string;
+  "connection-id": string;
+};
+
+export interface IBCChannelPortAndChannel {
+  "channel-id": string;
+  "port-id": string;
+}
+export interface IBCChannelInfo {
+  "chain-1": IBCChannelPortAndChannel;
+  "chain-2": IBCChannelPortAndChannel;
+  ordering: string;
+  version: string;
+  tags?: {
+    [key: string]: string;
+  }
+};
+
+export interface IBCInfo {
+  $schema: string;
+  "chain-1": IBCChainInfo;
+  "chain-2": IBCChainInfo;
+  channels: IBCChannelInfo[];
+}
+
+export const getIbcInfo = (ibc: IBCInfo[], fromChain: string, toChain: string): IBCInfo => {
+  return findInfo(ibc, fromChain, toChain)
+    || findInfo(ibc, toChain, fromChain);
+};
+
+export const getTransferChannel = (info: IBCInfo) => {
+  return info.channels.find(
+    channel => channel['chain-1']['port-id'] === 'transfer'
+      &&
+      channel['chain-2']['port-id'] === 'transfer'
+  );
+};
+
+export const getIbcDenom = (
+  ibc: IBCInfo[],
+  fromChain: string,
+  toChain: string,
+  assets: any[],
+  symbol: string
+): string => {
+  const ibcInfo = getIbcInfo(ibc, fromChain, toChain);
+  if (ibcInfo) {
+    const channel = getTransferChannel(ibcInfo);
+    if (!channel) {
+      return;
+    }
+    let channelInfo;
+    if (ibcInfo['chain-1']['chain-name'] === fromChain) {
+      channelInfo = channel['chain-1'];
+    } else {
+      channelInfo = channel['chain-2'];
+    }
+
+    const assetInfo = assets.find(({ chain_name }) => chain_name === toChain);
+    if (!assetInfo) {
+      return;
+    }
+    const token = assetInfo.assets.find(a => a.symbol === symbol);
+
+    if (!token) return;
+
+    return ibcDenom([
+      {
+        portId: channelInfo['port-id'],
+        channelId: channelInfo['channel-id']
+      }
+    ], token.base);
+  }
+};
