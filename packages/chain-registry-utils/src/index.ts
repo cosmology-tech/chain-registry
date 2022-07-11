b4cd6310bca3d4eaaab37307a7f476140f001dd9
Dan Lynch <pyramation@gmail.com>
2022-07-10 22:17:34 -0700

    parse IBC assets

--- packages/chain-registry-utils/src/index.ts
+++ packages/chain-registry-utils/src/index.ts
@@ -57,9 +57,9 @@ export interface IBCInfo {
   channels: IBCChannelInfo[];
 }
 
-export const getIbcInfo = (ibc: IBCInfo[], fromChain: string, toChain: string): IBCInfo => {
-  return findInfo(ibc, fromChain, toChain)
-    || findInfo(ibc, toChain, fromChain);
+export const getIbcInfo = (ibc: IBCInfo[], chain: string, counterparty: string): IBCInfo => {
+  return findInfo(ibc, chain, counterparty)
+    || findInfo(ibc, counterparty, chain);
 };
 
 export const getTransferChannel = (info: IBCInfo) => {
@@ -72,25 +72,25 @@ export const getTransferChannel = (info: IBCInfo) => {
 
 export const getIbcDenom = (
   ibc: IBCInfo[],
-  fromChain: string,
-  toChain: string,
+  chain: string,
+  counterparty: string,
   assets: any[],
   symbol: string
 ): string => {
-  const ibcInfo = getIbcInfo(ibc, fromChain, toChain);
+  const ibcInfo = getIbcInfo(ibc, chain, counterparty);
   if (ibcInfo) {
     const channel = getTransferChannel(ibcInfo);
     if (!channel) {
       return;
     }
     let channelInfo;
-    if (ibcInfo['chain-1']['chain-name'] === fromChain) {
+    if (ibcInfo['chain-1']['chain-name'] === chain) {
       channelInfo = channel['chain-1'];
     } else {
       channelInfo = channel['chain-2'];
     }
 
-    const assetInfo = assets.find(({ chain_name }) => chain_name === toChain);
+    const assetInfo = assets.find(({ chain_name }) => chain_name === counterparty);
     if (!assetInfo) {
       return;
     }
@@ -105,4 +105,111 @@ export const getIbcDenom = (
       }
     ], token.base);
   }
+};
+
+export const getIbcDenomByBase = (
+  ibc: IBCInfo[],
+  chain: string,
+  counterparty: string,
+  assets: any[],
+  base: string
+): string => {
+  const ibcInfo = getIbcInfo(ibc, chain, counterparty);
+  if (ibcInfo) {
+    const channel = getTransferChannel(ibcInfo);
+    if (!channel) {
+      return;
+    }
+    let channelInfo;
+    if (ibcInfo['chain-1']['chain-name'] === chain) {
+      channelInfo = channel['chain-1'];
+    } else {
+      channelInfo = channel['chain-2'];
+    }
+
+    const assetInfo = assets.find(({ chain_name }) => chain_name === counterparty);
+    if (!assetInfo) {
+      return;
+    }
+
+    return ibcDenom([
+      {
+        portId: channelInfo['port-id'],
+        channelId: channelInfo['channel-id']
+      }
+    ], base);
+  }
+};
+
+export const getIbcAssets = (
+  chainName: string,
+  ibc: IBCInfo[],
+  assets: any[]
+) => {
+
+  const chainIbcInfo = ibc.filter(i => {
+    return i['chain-1']['chain-name'] === chainName ||
+      i['chain-2']['chain-name'] === chainName
+  });
+
+  return chainIbcInfo.map(ibcInfo => {
+
+    const counterpartyIs = ibcInfo['chain-1']['chain-name'] === chainName ? 'chain-2' : 'chain-1';
+    const chainIs = ibcInfo['chain-1']['chain-name'] === chainName ? 'chain-1' : 'chain-2';
+
+    const counterparty = ibcInfo[counterpartyIs]['chain-name'];
+    const counterpartyIbc = ibcInfo[counterpartyIs];
+    const chainIbc = ibcInfo[chainIs];
+
+    const counterpartyAssets = assets.find(a => {
+      return a.chain_name === counterparty
+    });
+
+    if (!counterpartyAssets) {
+      console.warn('asset not found: ' + counterparty);
+      return;
+    }
+
+    const ibcAssets = counterpartyAssets
+      .assets.filter(a => !a.base.startsWith('cw20:'))
+      .map(asset => {
+        const denom = getIbcDenomByBase(
+          ibc,
+          chainName,
+          counterparty,
+          //
+          assets,
+          asset.base
+        );
+
+        const newAsset = {
+          ...asset
+        };
+        newAsset.base = denom;
+        newAsset.denom_units = newAsset.denom_units.map(unit => {
+          if (unit.denom === asset.base) {
+            const newUnit = {
+              ...unit
+            };
+            newUnit.denom = denom;
+            newUnit.aliases = [unit.denom];
+            return newUnit;
+          }
+          return unit;
+        });
+        return newAsset;
+      });
+    const channel = getTransferChannel(ibcInfo);
+    return {
+      chain: {
+        ...chainIbc,
+        ...channel[chainIs]
+      },
+      counterparty: {
+        ...counterpartyIbc,
+        ...channel[counterpartyIs]
+      },
+      assets: ibcAssets,
+    };
+  }).filter(Boolean);
 };
