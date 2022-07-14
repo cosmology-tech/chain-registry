3356e4c22ddadf432088f46c1d879465f2d80060
Dan Lynch <pyramation@gmail.com>
2022-07-13 23:14:45 -0700

    json

--- packages/chain-registry/types/index.d.ts
+++ packages/chain-registry/types/index.d.ts
@@ -5,4 +5,8 @@ declare namespace _default {
     export { ibc_assets };
 }
 export default _default;
+import assets from "./assets.json";
+import chains from "./chains.json";
+import ibc from "./ibc.json";
+import ibc_assets from "./ibc_assets.json";
 export { assets, chains, ibc, ibc_assets };
