4b4c69423dd01e85c5de611cfb39d9bea524aca4
Dan Lynch <pyramation@gmail.com>
2022-08-12 01:05:07 -0700

    json

--- packages/chain-registry/src/index.ts
+++ packages/chain-registry/src/index.ts
@@ -2,17 +2,10 @@ import assets from './assets.json';
 import chains from './chains.json';
 import ibc from './ibc.json';
 import ibc_assets from './ibc_assets.json';
-import { Chain } from './types';
 
-interface ChainRegistryModule {
-  chains: Chain[]
-}
-
-const chainRegistry: ChainRegistryModule = {
+export default {
   assets,
   chains,
   ibc,
   ibc_assets
-}
-
-export default chainRegistry;
+};
--- packages/chain-registry/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Chain } from './types';
-interface ChainRegistryModule {
-    chains: Chain[];
-}
-declare const chainRegistry: ChainRegistryModule;
-export default chainRegistry;
