cfcde2e810a9ab4da6da4caa42c195d072c000fb
Dan Lynch <pyramation@gmail.com>
2022-08-12 01:23:53 -0700

    types

---
+++ packages/chain-registry/types/index.d.ts
@@ -0,0 +1,18 @@
+import assets from './assets';
+import chains from './chains';
+import ibc from './ibc';
+import ibc_assets from './ibc_assets';
+
+export default {
+    assets,
+    chains,
+    ibc,
+    ibc_assets
+};
+
+export {
+    assets,
+    chains,
+    ibc,
+    ibc_assets
+};
