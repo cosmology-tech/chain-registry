731e9d581c45bd942d09bc83122ba0e88fbeeba8
Dan Lynch <pyramation@gmail.com>
2023-07-12 02:04:04 +0200

    types

--- packages/client/types/chain-info.d.ts
+++ packages/client/types/chain-info.d.ts
@@ -1,22 +1,19 @@
 import { AssetList, Chain, IBCInfo } from '@chain-registry/types';
-
 import { ChainRegistryFetcher } from './fetcher';
-
 export interface ChainInfoOptions {
-  chain_name: string;
-  fetcher: ChainRegistryFetcher;
+    chain_name: string;
+    fetcher: ChainRegistryFetcher;
 }
-
 export declare class ChainInfo {
-  chain_name: string;
-  fetcher: ChainRegistryFetcher;
-  protected _chain: Chain;
-  protected _asset_list: AssetList;
-  protected _asset_lists: AssetList[];
-  protected _ibc_data: IBCInfo[];
-  constructor(options: ChainInfoOptions);
-  refresh(): void;
-  get chain(): Chain;
-  get nativeAssetLists(): AssetList;
-  get assetLists(): AssetList[];
+    chain_name: string;
+    fetcher: ChainRegistryFetcher;
+    protected _chain: Chain;
+    protected _asset_list: AssetList;
+    protected _asset_lists: AssetList[];
+    protected _ibc_data: IBCInfo[];
+    constructor(options: ChainInfoOptions);
+    refresh(): void;
+    get chain(): Chain;
+    get nativeAssetLists(): AssetList;
+    get assetLists(): any[];
 }
--- packages/client/types/chain-util.d.ts
+++ packages/client/types/chain-util.d.ts
@@ -1,44 +1,23 @@
 import { Asset } from '@chain-registry/types';
-import type {
-  CoinDenom,
-  CoinGeckoUSD,
-  Exponent,
-  PriceHash
-} from '@chain-registry/utils';
-
+import type { CoinDenom, CoinGeckoUSD, Exponent, PriceHash } from '@chain-registry/utils';
 import { ChainInfo } from './chain-info';
-
 export interface ChainRegistryChainUtilOptions {
-  chain_name: string;
-  chain_info: ChainInfo;
+    chain_name: string;
+    chain_info: ChainInfo;
 }
-
 export declare class ChainRegistryChainUtil {
-  chain_name: string;
-  chain_info: ChainInfo;
-  all_Asset: Asset[];
-  constructor(options: ChainRegistryChainUtilOptions);
-  getAssetByDenom(denom: CoinDenom): Asset;
-  getDenomByCoinGeckoId(coinGeckoId: string): CoinDenom;
-  getSymbolByChainDenom(denom: CoinDenom): string;
-  getChainDenomBySymbol(token: string): CoinDenom;
-  getExponentByDenom(denom: CoinDenom): Exponent;
-  convertCoinGeckoPricesToDenomPriceMap(
-    prices: Record<string, CoinGeckoUSD>
-  ): PriceHash;
-  noDecimals(num: number | string): string;
-  convertBaseUnitsToDollarValue(
-    prices: PriceHash,
-    symbol: string,
-    amount: string | number
-  ): string;
-  convertDollarValueToDenomUnits(
-    prices: PriceHash,
-    symbol: string,
-    value: string | number
-  ): string;
-  convertBaseUnitsToDisplayUnits(
-    symbol: string,
-    amount: string | number
-  ): string;
+    chain_name: string;
+    chain_info: ChainInfo;
+    _all_Asset: Asset[];
+    constructor(options: ChainRegistryChainUtilOptions);
+    getAssetByDenom(denom: CoinDenom): Asset;
+    getDenomByCoinGeckoId(coinGeckoId: string): CoinDenom;
+    getSymbolByChainDenom(denom: CoinDenom): string;
+    getChainDenomBySymbol(token: string): CoinDenom;
+    getExponentByDenom(denom: CoinDenom): Exponent;
+    convertCoinGeckoPricesToDenomPriceMap(prices: Record<string, CoinGeckoUSD>): PriceHash;
+    noDecimals(num: number | string): string;
+    convertBaseUnitsToDollarValue(prices: PriceHash, symbol: string, amount: string | number): string;
+    convertDollarValueToDenomUnits(prices: PriceHash, symbol: string, value: string | number): string;
+    convertBaseUnitsToDisplayUnits(symbol: string, amount: string | number): string;
 }
