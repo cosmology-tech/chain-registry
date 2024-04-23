5905a38e4518496f306cd7aa28ce37a3d4909795
Dan Lynch <pyramation@gmail.com>
2024-04-22 17:57:25 -0700

    output fixtures

---
+++ __output__/camel/versions.schema.ts
@@ -0,0 +1,33 @@
+export interface Versions {
+  schema?: string;
+  chainName: string;
+  versions: {
+    name: string;
+    tag?: string;
+    height?: number;
+    proposal?: number;
+    previousVersionName?: string;
+    nextVersionName?: string;
+    recommendedVersion?: string;
+    compatibleVersions?: string[];
+    cosmosSdkVersion?: string;
+    consensus?: {
+      type: "tendermint" | "cometbft";
+      version?: string;
+    };
+    cosmwasmVersion?: string;
+    cosmwasmEnabled?: boolean;
+    cosmwasmPath?: string;
+    ibcGoVersion?: string;
+    goVersion?: string;
+    icsEnabled?: ("ics20-1" | "ics27-1" | "mauth")[];
+    binaries?: {
+      "linux/amd64"?: string;
+      "linux/arm64"?: string;
+      "darwin/amd64"?: string;
+      "darwin/arm64"?: string;
+      "windows/amd64"?: string;
+      "windows/arm64"?: string;
+    };
+  }[];
+}
