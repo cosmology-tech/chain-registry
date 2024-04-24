5281ca47f6f7fe513f82b5e7159f7d6ab2131abb
Dan Lynch <pyramation@gmail.com>
2024-04-24 01:14:29 -0700

    codegen

--- v2/types/src/index.ts
+++ v2/types/src/index.ts
@@ -1,3 +1,5 @@
 export { Asset, AssetList } from './assetlist.schema';
 export { Chain } from './chain.schema';
 export { IBCData } from './ibc_data.schema';
+export { MemoKeys } from './memo_keys.schema';
+export { Versions } from './versions.schema';
