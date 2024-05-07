a31e9caa3265073d3ede209e0e12118bdc352838
Lucas Jiang <secretjiang@gmail.com>
2024-05-07 23:14:14 +0800

    export more types for interface

--- v2/interfaces/src/index.ts
+++ v2/interfaces/src/index.ts
@@ -1,5 +1,5 @@
-export { Asset, AssetList } from './assetlist.schema';
-export { Chain } from './chain.schema';
-export { IBCData } from './ibc_data.schema';
+export { Asset, AssetList, DenomUnit, Pointer, IbcTransition, IbcCw20Transition, NonIbcTransition } from './assetlist.schema';
+export { Chain, Peer, Endpoint, Explorer, FeeToken, StakingToken } from './chain.schema';
+export { IBCData, ChainOperatorInfo, ChainInfo, ChannelInfo } from './ibc_data.schema';
 export { MemoKeys } from './memo_keys.schema';
 export { Versions } from './versions.schema';
