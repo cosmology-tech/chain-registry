451767b35538615d7ab49be17e0bf98c7ce5afd1
cansijyun <42692305@qq.com>
2024-06-01 08:54:32 +0800

    fix: restore version of v2-keplr and add @ts-ignore to v2-keplr/test-utils

--- v2/keplr/test-utils/index.ts
+++ v2/keplr/test-utils/index.ts
@@ -1,11 +1,11 @@
-// @ts-nocheck 
 import { AssetList, Chain, IBCData } from '@chain-registry/v2-types';
 import { getFixturesRegistry } from '@chain-registry/workflows';
 
 const fixturesReg = getFixturesRegistry();
-
+// @ts-ignore
 export const assets: AssetList[] = fixturesReg.assetLists
 // TODO update fixtures, they may be missing properties and/or old schema
+// @ts-ignore
 export const chains: Chain[] = fixturesReg.chains;
-
+// @ts-ignore
 export const ibc: IBCData[] = fixturesReg.ibcData;
