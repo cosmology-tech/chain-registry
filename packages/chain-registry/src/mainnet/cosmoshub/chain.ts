3223d3013cac6b48d6efda945258d972c27d4287
pyramation <pyramation@users.noreply.github.com>
2024-07-05 01:56:10 +0000

    build 🛠 build-20240705-015610

--- packages/chain-registry/src/mainnet/cosmoshub/chain.ts
+++ packages/chain-registry/src/mainnet/cosmoshub/chain.ts
@@ -27,7 +27,7 @@ const info: Chain = {
       }]
   },
   codebase: {
-    cosmos_sdk_version: 'v0.47.13-ics-lsm'
+    cosmos_sdk_version: 'v0.47.15-ics-lsm'
   },
   logo_URIs: {
     png: 'https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/images/atom.png',
