d5466b9579a4ce584e2479859dcd6c7a74ad108a
Davide Segullo <davidesegullo@gmail.com>
2024-09-14 11:37:32 +0200

    docs: :memo: add infer chain names example

--- packages/chain-registry/examples/examples.ts
+++ packages/chain-registry/examples/examples.ts
@@ -3,25 +3,27 @@ import { chain as chain_akash } from '../src/mainnet/akash';
 
 console.log(chain_akash.apis);
 
-// import single chain from root, good for tree shaking
-import { akash, akashtestnet } from '../src';
-
-console.log(akash.chain.apis, akashtestnet.chain.apis);
-
-// backward compatibility using "all.ts": get all chains at once:
-import { chains as all_chains } from '../src/all';
-
-console.log(all_chains.find((chain) => chain.chain_name === 'akash'));
-
 // backward compatibility by default: get all chains at once:
 import { chains } from '../src';
 
 console.log(chains.find((chain) => chain.chain_name === 'akash'));
 
 // made bundle smaller: get all chains only in mainnet:
-import { chains as chains_main } from '../src/mainnet/all';
+import { chains as chains_main } from '../src/mainnet';
 
 console.log(chains_main.find((chain) => chain.chain_name === 'akash'));
+
+// infer types thanks to `satisfies` operator
+import { chain as chain_cosmoshub } from '../src/mainnet/cosmoshub';
+import { chain as chain_osmosis } from '../src/mainnet/osmosis';
+
+const supportedChains = [
+  chain_osmosis,
+  chain_cosmoshub,
+];
+
+export type Chains = typeof supportedChains[number]['chain_name']
+//          ^ output: "osmosis" | "cosmoshub"
 
 // made bundle even smaller: get all chains only in mainnet
 // and with minimal info customized for dropbox:
