1f233ddb96f47f7255274596a15cb5f2c5ac0908
Dan Lynch <pyramation@gmail.com>
2024-09-30 16:18:55 -0700

    v2 types codegen typegen

--- v2/interfaces/src/versions.schema.ts
+++ v2/interfaces/src/versions.schema.ts
@@ -1,3 +1,46 @@
+export type Repo = string;
+export type Version = string;
+export type Tag = string;
+export interface Sdk {
+  type: "cosmos" | "penumbra" | "other";
+  version?: Version;
+  repo?: Repo;
+  tag?: Tag;
+}
+export interface Ibc {
+  type: "go" | "rust" | "other";
+  version?: Version;
+  repo?: Repo;
+  tag?: Tag;
+  ics_enabled?: ("ics20-1" | "ics27-1" | "mauth")[];
+}
+export interface Cosmwasm {
+  version?: Version;
+  repo?: Repo;
+  tag?: Tag;
+  enabled?: boolean;
+  path?: string;
+}
+export interface Consensus {
+  type: "tendermint" | "cometbft" | "sei-tendermint";
+  version?: Version;
+  repo?: Repo;
+  tag?: Tag;
+}
+export interface Language {
+  type: "go" | "rust" | "solidity" | "other";
+  version?: Version;
+  repo?: Repo;
+  tag?: Tag;
+}
+export interface Binaries {
+  "linux/amd64"?: string;
+  "linux/arm64"?: string;
+  "darwin/amd64"?: string;
+  "darwin/arm64"?: string;
+  "windows/amd64"?: string;
+  "windows/arm64"?: string;
+}
 export interface Versions {
   $schema?: string;
   chain_name: string;
@@ -11,23 +54,16 @@ export interface Versions {
     recommended_version?: string;
     compatible_versions?: string[];
     cosmos_sdk_version?: string;
-    consensus?: {
-      type: "tendermint" | "cometbft";
-      version?: string;
-    };
+    sdk?: Sdk;
+    consensus?: Consensus;
     cosmwasm_version?: string;
     cosmwasm_enabled?: boolean;
     cosmwasm_path?: string;
+    cosmwasm?: Cosmwasm;
     ibc_go_version?: string;
-    go_version?: string;
+    ibc?: Ibc;
+    language?: Language;
     ics_enabled?: ("ics20-1" | "ics27-1" | "mauth")[];
-    binaries?: {
-      "linux/amd64"?: string;
-      "linux/arm64"?: string;
-      "darwin/amd64"?: string;
-      "darwin/arm64"?: string;
-      "windows/amd64"?: string;
-      "windows/arm64"?: string;
-    };
+    binaries?: Binaries;
   }[];
 }
