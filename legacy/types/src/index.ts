39e3e047a1b4b779ef917cda672305ab2e13dbf3
Dan Lynch <pyramation@gmail.com>
2024-09-30 16:30:27 -0700

    typegen for v1 legacy types

--- legacy/types/src/index.ts
+++ legacy/types/src/index.ts
@@ -1,4 +1,5 @@
-export * from './assets';
-export * from './chains';
-export * from './ibc';
+export { Asset, AssetList } from './assetlist.schema';
+export { Chain } from './chain.schema';
+// temporarily use IBCInfo until v2
+export { IBCData as IBCInfo } from './ibc_data.schema';
 export * from './registry';
