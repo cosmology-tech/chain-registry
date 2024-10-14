ed849557d7fd0e79adc93097a1539409f8a0590b
Lucas Jiang <secretjiang@gmail.com>
2024-10-14 19:23:00 +0800

    add test

---
+++ v2/packages/keplr/__tests__/keplr.test.ts
@@ -0,0 +1,19 @@
+import { chainRegistryChainToKeplr } from '../src/';
+import { chains, assetLists } from '@chain-registry/v2/mainnet'
+
+it('keplr', () => {
+  const chain = chains.find((chain) => chain.chainName === 'osmosis')!;
+  const config = chainRegistryChainToKeplr(chain, assetLists);
+  expect(config).toMatchSnapshot();
+});
+
+it('getExplorer', () => {
+  const chain = chains.find((chain) => chain.chainName === 'osmosis')!;
+  const address = chain.apis!.rest![1]!.address!;
+
+  const config = chainRegistryChainToKeplr(chain, assetLists, {
+    getExplorer: () => 'https://myexplorer.com',
+    getRestEndpoint: (_chain) => address
+  });
+  expect(config).toMatchSnapshot();
+});
