5f1f13f12f9ab9f13ed85aa8996a12e8a2828dd8
danny.kehu <hu-ke@hotmail.com>
2024-11-06 17:26:38 +0800

    added build-alive script for v1

--- v1/packages/chain-registry/scripts/build-alive.ts
+++ v1/packages/chain-registry/scripts/build-alive.ts
@@ -80,34 +80,45 @@ function updateCategoryFiles(categoryPath: string): void {
     fs.writeFileSync(chainsFilePath, content, 'utf-8');
   }
 
-  // Update the contents of asset-list.ts
-  const assetListFilePath = path.join(categoryPath, 'asset-lists.ts');
-  if (fs.existsSync(assetListFilePath)) {
-    let content = fs.readFileSync(assetListFilePath, 'utf-8');
+  // Update the contents of assets.ts
+  const assetsFilePath = path.join(categoryPath, 'assets.ts');
+  if (fs.existsSync(assetsFilePath)) {
+    let content = fs.readFileSync(assetsFilePath, 'utf-8');
     killedModules.forEach(module => {
       // Remove imports for modules that do not meet the conditions
       const regex = new RegExp(`import \\* as _${module} from '\\.${path.sep}${module}';[\\s\\S]*?\\n`);
       content = content.replace(regex, ''); // Remove import statement
-      // content = content.replace(new RegExp(`_\\${module}\\.assetList(\\s*,?\\s*)`, 'g'), ''); // Remove assetList array reference
-      const regex2 = new RegExp(`\\s*_(${module}).assetList,?\\s*`, 'g');
+      const regex2 = new RegExp(`\\s*_(${module}).assets,?\\s*`, 'g');
       content = content.replace(regex2, '');
     });
-    fs.writeFileSync(assetListFilePath, content, 'utf-8');
+    fs.writeFileSync(assetsFilePath, content, 'utf-8');
   }
 
-  // Update the contents of ibc-data.ts
-  const ibcDataFilePath = path.join(categoryPath, 'ibc-data.ts');
-  if (fs.existsSync(ibcDataFilePath)) {
-    let content = fs.readFileSync(ibcDataFilePath, 'utf-8');
+  // Update the contents of ibc.ts
+  const ibcFilePath = path.join(categoryPath, 'ibc.ts');
+  if (fs.existsSync(ibcFilePath)) {
+    let content = fs.readFileSync(ibcFilePath, 'utf-8');
     killedModules.forEach(module => {
       // Remove imports for modules that do not meet the conditions
       const regex = new RegExp(`import \\* as _${module} from '\\.${path.sep}${module}';[\\s\\S]*?\\n`);
       content = content.replace(regex, ''); // Remove import statement
       // content = content.replace(new RegExp(`\\.{3}_\\${module}\\.ibcData(\\s*,?\\s*)`, 'g'), ''); // Remove ibcData array reference
-      const regex2 = new RegExp(`\\s*..._(${module}).ibcData,?\\s*`, 'g');
+      const regex2 = new RegExp(`\\s*..._(${module}).ibc,?\\s*`, 'g');
       content = content.replace(regex2, '');
     });
-    fs.writeFileSync(ibcDataFilePath, content, 'utf-8');
+    fs.writeFileSync(ibcFilePath, content, 'utf-8');
+  }
+
+  // Update the contents of named.ts
+  const namedFilePath = path.join(categoryPath, 'named.ts');
+  if (fs.existsSync(namedFilePath)) {
+    let content = fs.readFileSync(namedFilePath, 'utf-8');
+    killedModules.forEach(module => {
+      // Remove imports for modules that do not meet the conditions
+      const regex = new RegExp(`export \\* as _?${module} from '\\.${path.sep}${module}';[\\s\\S]*?\\n`);
+      content = content.replace(regex, ''); // Remove import statement
+    });
+    fs.writeFileSync(namedFilePath, content, 'utf-8');
   }
 }
 
