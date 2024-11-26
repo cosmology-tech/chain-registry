de3ea837d9c7271d1fb5fc40c89a1fee09529863
pyramation <pyramation@users.noreply.github.com>
2024-11-26 03:00:33 +0000

    build 🛠 build-20241126-030033

--- v2/packages/chain-registry/src/mainnet/int3face/chain.ts
+++ v2/packages/chain-registry/src/mainnet/int3face/chain.ts
@@ -14,13 +14,43 @@ const info: Chain = {
   keyAlgos: ['secp256k1'],
   slip44: 118,
   fees: {
-    feeTokens: [{
+    feeTokens: [
+      {
         denom: 'uint3',
-        fixedMinGasPrice: 0.0025,
-        lowGasPrice: 0.0025,
+        fixedMinGasPrice: 0.01,
+        lowGasPrice: 0.01,
         averageGasPrice: 0.025,
         highGasPrice: 0.04
-      }]
+      },
+      {
+        denom: 'factory/int31zlefkpe3g0vvm9a4h0jf9000lmqutlh99h7fsd/bitcoin-btc',
+        fixedMinGasPrice: 1e-7,
+        lowGasPrice: 1e-7,
+        averageGasPrice: 0.0000025,
+        highGasPrice: 0.000004
+      },
+      {
+        denom: 'factory/int31zlefkpe3g0vvm9a4h0jf9000lmqutlh99h7fsd/bitcoin-cash-bch',
+        fixedMinGasPrice: 0.000001,
+        lowGasPrice: 0.000001,
+        averageGasPrice: 0.000025,
+        highGasPrice: 0.00004
+      },
+      {
+        denom: 'factory/int31zlefkpe3g0vvm9a4h0jf9000lmqutlh99h7fsd/litecoin-ltc',
+        fixedMinGasPrice: 0.000001,
+        lowGasPrice: 0.000001,
+        averageGasPrice: 0.000025,
+        highGasPrice: 0.00004
+      },
+      {
+        denom: 'factory/int31zlefkpe3g0vvm9a4h0jf9000lmqutlh99h7fsd/dogecoin-doge',
+        fixedMinGasPrice: 0.001,
+        lowGasPrice: 0.001,
+        averageGasPrice: 0.0025,
+        highGasPrice: 0.004
+      }
+    ]
   },
   staking: {
     stakingTokens: [{
@@ -71,12 +101,16 @@ const info: Chain = {
     rest: [{
         address: 'https://api.mainnet.int3face.zone',
         provider: 'Int3face.io'
+      }],
+    grpc: [{
+        address: 'grpc.mainnet.int3face.zone:443',
+        provider: 'Int3face.io'
       }]
   },
   explorers: [{
       kind: 'ping.pub',
       url: 'https://explorer.int3face.zone',
-      txPage: 'https://explorer.int3face.zone/tx/${txHash}'
+      txPage: 'https://explorer.int3face.zone/int3face-1/tx/${txHash}'
     }],
   images: [{
       imageSync: {
