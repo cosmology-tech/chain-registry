5b54901cf078a4358512e25671080e7e2e1b8fa3
Govard Barkhatov <gbarkhatov@gmail.com>
2025-01-14 16:03:55 +0200

    update babylon testnet 5

--- v1/packages/chain-registry/src/testnet/babylontestnet/chain.ts
+++ v1/packages/chain-registry/src/testnet/babylontestnet/chain.ts
@@ -1,13 +1,14 @@
 import { Chain } from '@chain-registry/types';
+
 const info: Chain = {
   $schema: '../../chain.schema.json',
   chain_name: 'babylontestnet',
   status: 'live',
-  website: 'https://www.babylonchain.io',
+  website: 'https://babylonlabs.io/',
   network_type: 'testnet',
-  pretty_name: 'Babylon Testnet 3',
+  pretty_name: 'Babylon Phase-2 Testnet',
   chain_type: 'cosmos',
-  chain_id: 'bbn-test3',
+  chain_id: 'bbn-test-5',
   pre_fork_chain_name: 'babylontestnet1',
   bech32_prefix: 'bbn',
   slip44: 118,
@@ -16,41 +17,49 @@ const info: Chain = {
   key_algos: ['secp256k1'],
   fees: {
     fee_tokens: [{
-        denom: 'ubbn'
-      }]
+      denom: 'ubbn',
+      fixed_min_gas_price: 0,
+      low_gas_price: 0.007,
+      average_gas_price: 0.007,
+      high_gas_price: 0.01
+    }]
   },
   staking: {
     staking_tokens: [{
-        denom: 'ubbn'
-      }]
+      denom: 'ubbn'
+    }]
   },
   codebase: {
-
+    git_repo: 'https://github.com/babylonlabs-io/babylon/releases',
+    recommended_version: 'v0.18.2',
+    compatible_versions: ['v0.18.2'],
   },
   logo_URIs: {
-    svg: 'https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/babylontestnet/images/logo.svg'
+    png: 'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/bbn-test/chain.png'
   },
   apis: {
     rpc: [{
-        address: 'rpc.testnet3.babylonchain.io:443',
-        provider: 'Babylon foundation'
-      }],
+      address: 'https://babylon-testnet-rpc.nodes.guru',
+      provider: 'NodesGuru'
+    }],
     rest: [{
-        address: 'lcd.testnet3.babylonchain.io:443',
-        provider: 'Babylon foundation'
-      }],
+      address: 'https://babylon-testnet-api.nodes.guru',
+      provider: 'NodesGuru'
+    }],
     grpc: []
   },
   explorers: [{
-      kind: 'babylonscan',
-      url: 'https://babylonscan.io'
-    }, {
-      kind: 'explorers.guru',
-      url: 'https://testnet.babylon.explorers.guru',
-      tx_page: 'https://testnet.babylon.explorers.guru/transaction/${txHash}'
-    }],
+    kind: 'babylonscan',
+    url: 'https://babylon-testnet.l2scan.co/tx/${txHash}',
+    tx_page: 'https://babylon-testnet.l2scan.co/tx/${txHash}'
+  }, {
+    kind: 'explorers.guru',
+    url: 'https://testnet.babylon.explorers.guru',
+    tx_page: 'https://testnet.babylon.explorers.guru/transaction/${txHash}'
+  }],
   images: [{
-      svg: 'https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/babylontestnet/images/logo.svg'
-    }]
+    png: 'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/bbn-test/chain.png'
+  }]
 };
+
 export default info;
